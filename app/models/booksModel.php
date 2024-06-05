c6400f4fcea1bbb8c97d8f5812db2c7329bdd86b
Ryan <ryan.senterre@gmail.com>
2024-06-05 17:50:16 +0200

    JOIN authors

--- app/models/booksModel.php
+++ app/models/booksModel.php
@@ -4,9 +4,10 @@ namespace App\Models\BooksModel;
 use \PDO;
 
 function findAll(PDO $connexion): array {
-    $sql = "SELECT *
-    FROM books
-    ORDER BY created_at DESC
+    $sql = "SELECT *, b.id AS BooksID, a.id AS authorID
+    FROM books b
+    INNER JOIN authors a ON b.author_id = a.id
+    ORDER BY b.created_at DESC
     LIMIT 3;";
     return $connexion->query($sql)->fetchAll(PDO::FETCH_ASSOC);
 }
