b31d4742fd7dec1c4efd4bc6ac3e8f2d435dbc34
Ryan <ryan.senterre@gmail.com>
2024-06-05 20:04:25 +0200

    Route Books._index
    Avec le partials _index

--- app/routers/index.php
+++ app/routers/index.php
@@ -1,11 +1,18 @@
 <?php
 // ROUTER PRINCIPAL
 
-
+// ROUTE DES BOOKS 
+// PATERN: /?photos
+// CTRL: photosController
+// ACTION: indexAction
+if(isset($_GET['books'])) :
+    include_once '../app/controllers/booksController.php';
+    \App\Controllers\BooksController\indexAction($connexion);
 // ROUTE PAR DEFAUT
 // PATERN
 // CTRL pagesController
 //ACTION homeAction
-
+else: 
 include_once '../app/controllers/pagesController.php';
 \App\Controllers\PagesController\homeAction($connexion);
+endif;
