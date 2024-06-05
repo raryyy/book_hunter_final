15c3c8c7be46f0e1f5bfcdd9f9f30ed895b62ee7
Ryan <ryan.senterre@gmail.com>
2024-06-05 18:14:38 +0200

    Categories._index

---
+++ app/views/categories/_index.php
@@ -0,0 +1,14 @@
+<div class="bg-gray-700 rounded-lg shadow-lg p-4">
+<h2 class="font-bold text-lg mb-4">Categories</h2>
+          <ul class="list-reset">
+            <?php  
+            include_once '../app/models/categoriesModel.php';
+            $categories = \App\Models\CategoriesModel\findAll($connexion);
+            foreach($categories as $category) : 
+            ?>
+            <li>
+              <a class="text-gray-300 hover:text-white" href="#"><?php echo $category['name'] ?> </a>
+            </li>
+            <?php  endforeach; ?>
+          </ul>
+        </div>
--- app/views/templates/partials/_aside.php
+++ app/views/templates/partials/_aside.php
@@ -1,23 +1,5 @@
-<aside class="w-full md:w-1/4 p-3">
-        <div class="bg-gray-700 rounded-lg shadow-lg p-4">
-          <h2 class="font-bold text-lg mb-4">Categories</h2>
-          <ul class="list-reset">
-            <li>
-              <a class="text-gray-300 hover:text-white" href="#">Action</a>
-            </li>
-            <li>
-              <a class="text-gray-300 hover:text-white" href="#">Comédie</a>
-            </li>
-            <li>
-              <a class="text-gray-300 hover:text-white" href="#">Drame</a>
-            </li>
-            <li>
-              <a class="text-gray-300 hover:text-white" href="#"
-                >Science-fiction</a
-              >
-            </li>
-          </ul>
-        </div>
+        <aside class="w-full md:w-1/4 p-3">
+        <?php include '../app/views/categories/_index.php'; ?>
         <div class="bg-gray-700 rounded-lg shadow-lg p-4 mt-4">
           <h2 class="font-bold text-lg mb-4">Tags</h2>
           <div class="tag-cloud">
